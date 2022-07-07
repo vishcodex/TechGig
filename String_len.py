b0b72496b0203174d312f539f61f04749c903752
Putta, Vishnu <vishnu.reddy@anthem.com>
2022-07-07 17:32:57 -0500

    Solved Tech Gig puzzles

---
+++ String_len.py
@@ -0,0 +1,25 @@
+# String len (100 Marks)
+# You just need to take string input from stdin and print the length of the string.
+
+# Input Format
+# You will be taking a string as an input from stdin.
+
+# Constraints
+# 1 <= |S| <= 10000
+
+# Output Format
+# You need to print the length of the string to the stdout.
+
+
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+
+ # Write code here 
+    value = input()
+
+    print(len(value))
+
+main()
+
