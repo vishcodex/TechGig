6cd9df7310e0b93de98adec83fcd6b1fabfc65b3
Putta, Vishnu <vishnu.reddy@anthem.com>
2022-07-08 17:10:28 -0500

    Solved Tech Gig puzzles

---
+++ String_replace.py
@@ -0,0 +1,23 @@
+# You just need to take string input from stdin and print the string in which all whitespaces have been stripped from the end of the string.
+
+# Input Format
+# You will be taking a string as an input from stdin.
+
+# Constraints
+# 1 <= |S| <= 10000
+
+# Output Format
+# You need to print the string to the stdout.
+
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+
+ # Write code here 
+    value = input()
+
+    print(value.strip())
+
+main()
+
---
+++ String_right_strip.py
@@ -0,0 +1,13 @@
+
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+
+ # Write code here 
+    value = input()
+
+    print(value.rstrip())
+
+main()
+
